593ffe36e187cdd5fa0ada80b0b5a06eb7be478e
acem0301 <acem0301@gmail.com>
2021-08-01 23:34:28 -0400

    Avances

---
+++ src/components/Badge.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+class Badge extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <img src="" alt="Logo Conferencia" />
+        </div>
+
+        <div>
+          <h1>
+            Adriana <br /> Estigarribia
+          </h1>
+        </div>
+
+        <div>
+          <img src="" alt="Avatar" />
+          <p>FrontEnd Engineer</p>
+          <p>@acem0301</p>
+        </div>
+
+        <div>#platziConf</div>
+      </div>
+    );
+  }
+}
+
+export default Badge;
